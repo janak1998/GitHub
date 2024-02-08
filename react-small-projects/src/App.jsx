dc814edbe46b400260b8386ea514a9f709fd5ccd
janak bohra <janakbohra29@gmail.com>
2024-02-08 18:02:18 +0530

    star rating component

--- react-small-projects/src/App.jsx
+++ react-small-projects/src/App.jsx
@@ -1,11 +1,14 @@
 import Accordian from "./components/accordian";
 import { RandomColor } from "./components/random-color";
+import StarRating from "./components/star-rating";
 function App() {
   return (
     <>
       {/* <Accordian /> */}
 
-      <RandomColor />
+      {/* <RandomColor /> */}
+
+      <StarRating noOfStars={5} />
     </>
   );
 }
