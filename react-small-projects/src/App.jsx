d1586b93096fa6b8787831f539fc0ac5c46074ab
janak bohra <janakbohra29@gmail.com>
2024-02-09 20:10:42 +0530

    image slider

--- react-small-projects/src/App.jsx
+++ react-small-projects/src/App.jsx
@@ -1,4 +1,5 @@
 import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
 import { RandomColor } from "./components/random-color";
 import StarRating from "./components/star-rating";
 function App() {
@@ -8,7 +9,13 @@ function App() {
 
       {/* <RandomColor /> */}
 
-      <StarRating noOfStars={5} />
+      {/* <StarRating noOfStars={5} /> */}
+
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   );
 }
