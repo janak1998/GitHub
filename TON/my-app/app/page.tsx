022e0b48422d1a5663d4dc369cbb20adc499dd76
janak bohra <janakbohra29@gmail.com>
2024-08-24 16:01:20 +0530

    Made Ui changes

--- TON/my-app/app/page.tsx
+++ TON/my-app/app/page.tsx
@@ -17,7 +17,16 @@ export default function Home() {
 
   const [userData, setUserData] = useState<UserData | null>(null);
 
-  useEffect(()=> {
+  const userData2 = {
+    id: 123,
+    first_name: 'janak',
+    last_name: 'bohra',
+    username: 'janaxbohra',
+    language_code: 'en',
+    is_premium: false,
+  }
+
+  useEffect(() => {
     if (WebApp.initDataUnsafe.user) {
       setUserData(WebApp.initDataUnsafe.user as unknown as UserData)
     }
@@ -25,21 +34,21 @@ export default function Home() {
 
 
   return (
-    <main className="p-4">
+    <main className="p-4 flex flex-col justify-center items-center text-white bg-black h-[100vh] font-mono">
       {
-        userData ? (
+        userData2 ? (
           <>
-          <h1 className="text-2xl font-bold mb-4">User Data</h1>
-          <ul>
-            <li>
-              ID: {userData.id}
-            </li>
-            <li> First Name: {userData.first_name}</li>
-            <li> Last Name: {userData.last_name}</li>
-            <li> Username: {userData.username}</li>
-            <li> Language Code: {userData.language_code}</li>
-            <li> Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
-          </ul>
+            <h1 className="text-4xl font-bold mb-16">User Data</h1>
+            <ul className="flex flex-col gap-4">
+              <li>
+                ID: {userData2.id}
+              </li>
+              <li> First Name: {userData2.first_name}</li>
+              <li> Last Name: {userData2.last_name}</li>
+              <li> Username: {userData2.username}</li>
+              <li> Language Code: {userData2.language_code}</li>
+              <li> Is Premium: {userData2.is_premium ? 'Yes' : 'No'}</li>
+            </ul>
           </>
         ) : (
           <div>Loading...</div>
