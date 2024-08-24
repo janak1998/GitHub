d0d49872d85cda1f2f409309b87e6f5ab10ad688
janak bohra <janakbohra29@gmail.com>
2024-08-24 16:04:20 +0530

    dynamic data added

--- TON/my-app/app/page.tsx
+++ TON/my-app/app/page.tsx
@@ -17,14 +17,14 @@ export default function Home() {
 
   const [userData, setUserData] = useState<UserData | null>(null);
 
-  const userData2 = {
-    id: 123,
-    first_name: 'janak',
-    last_name: 'bohra',
-    username: 'janaxbohra',
-    language_code: 'en',
-    is_premium: false,
-  }
+  // const userData = {
+  //   id: 123,
+  //   first_name: 'john',
+  //   last_name: 'doe',
+  //   username: 'johndoe',
+  //   language_code: 'en',
+  //   is_premium: false,
+  // }
 
   useEffect(() => {
     if (WebApp.initDataUnsafe.user) {
@@ -36,18 +36,18 @@ export default function Home() {
   return (
     <main className="p-4 flex flex-col justify-center items-center text-white bg-black h-[100vh] font-mono">
       {
-        userData2 ? (
+        userData ? (
           <>
             <h1 className="text-4xl font-bold mb-16">User Data</h1>
             <ul className="flex flex-col gap-4">
               <li>
-                ID: {userData2.id}
+                ID: {userData.id}
               </li>
-              <li> First Name: {userData2.first_name}</li>
-              <li> Last Name: {userData2.last_name}</li>
-              <li> Username: {userData2.username}</li>
-              <li> Language Code: {userData2.language_code}</li>
-              <li> Is Premium: {userData2.is_premium ? 'Yes' : 'No'}</li>
+              <li> First Name: {userData.first_name}</li>
+              <li> Last Name: {userData.last_name}</li>
+              <li> Username: {userData.username}</li>
+              <li> Language Code: {userData.language_code}</li>
+              <li> Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
             </ul>
           </>
         ) : (
