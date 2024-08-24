6c372c6d207f1060eb31bea1d2d13f5ea20fe22f
janak bohra <janakbohra29@gmail.com>
2024-08-24 14:45:36 +0530

    First Telegram mini app

---
+++ TON/my-app/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Script from "next/script";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Telegram Mini App",
+  description: "A simple telegram Mini App using next.js 14",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8"></meta>
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
