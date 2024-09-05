b39fe4d7a2cb1dd1aabf66b1fc0b61b367c052cb
janak bohra <janakbohra29@gmail.com>
2024-09-05 12:57:37 +0530

    build error resolved

--- TON/jwt-auth/app/api/auth/route.ts
+++ TON/jwt-auth/app/api/auth/route.ts
@@ -1,16 +1,29 @@
-import { validateTelegramWebAppData } from "@/utils/telegramAuth";
 import { NextResponse } from "next/server";
+import { validateTelegramWebAppData } from "@/utils/telegramAuth";
+import { cookies } from "next/headers";
+import { encrypt, SESSION_DURATION } from "@/utils/session";
 
-export async function POST(request: Request){
-  const { initData} = await request.json();
+export async function POST(request: Request) {
+  const { initData } = await request.json();
 
   const validationResult = validateTelegramWebAppData(initData);
 
+  if (validationResult.validatedData) {
+    console.log("Validation result: ", validationResult);
+    const user = { telegramId: validationResult.user.id };
 
-  if(validationResult.validatedData){
-    console.log('validation result: ',validationResult)
-    const user = {telegramId: validationResult.user.id}
-  }else {
-    return NextResponse.json({ message: validationResult.message}, { status:401})
+    // Create a new session
+    const expires = new Date(Date.now() + SESSION_DURATION);
+    const session = await encrypt({ user, expires });
+
+    // Save the session in a cookie
+    cookies().set("session", session, { expires, httpOnly: true });
+
+    return NextResponse.json({ message: "Authentication successful" });
+  } else {
+    return NextResponse.json(
+      { message: validationResult.message },
+      { status: 401 }
+    );
   }
 }
