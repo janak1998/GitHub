7ec80ac0f393ccc9db40387588a33ec264c5aa47
janak bohra <janakbohra29@gmail.com>
2024-10-02 10:05:40 +0530

    added footer links

--- tgg/components/Footer.js
+++ tgg/components/Footer.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
@@ -5,9 +6,18 @@ const Footer = () => {
     <footer className="footer footer-center bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded p-10">
       <div className="container px-4 py-10 mx-auto flex flex-col gap-8">
         <nav className="cursor-pointer flex justify-center items-center gap-4">
-          <a className="link link-hover hover:text-blue-500">Services</a>
-          <a className="link link-hover hover:text-blue-500">Pricing</a>
-          <a className="link link-hover hover:text-blue-500">About</a>
+          <Link
+            href="/services"
+            className="link link-hover hover:text-blue-500"
+          >
+            Services
+          </Link>
+          <Link href="/pricing" className="link link-hover hover:text-blue-500">
+            Pricing
+          </Link>
+          <Link href="/about" className="link link-hover hover:text-blue-500">
+            About
+          </Link>
         </nav>
         <nav>
           <div className="flex justify-center gap-8">
